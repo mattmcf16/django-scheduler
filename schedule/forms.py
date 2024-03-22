28faa7d4c7d8b150786d52ab9c537ba13c6e4157
mattmcf16 <mattmcf16@yahoo.com>
2024-03-22 17:28:52 -0400

    Whatever

--- schedule/forms.py
+++ schedule/forms.py
@@ -35,32 +35,6 @@ class EventForm(SpanForm):
         model = Event
         exclude = ("creator", "created_on", "calendar")
 
-class EventForm(forms.ModelForm):
-    start = forms.SplitDateTimeField(label=_("start"))
-    end = forms.SplitDateTimeField(
-        label=_("end"), help_text=_("The end time must be later than start time.")
-    )
-    end_recurring_period = forms.DateTimeField(
-        label=_("End recurring period"),
-        help_text=_("This date is ignored for one time only events."),
-        required=False,
-    )
-    # Add this line to include a field for selecting multiple calendars
-
-    def clean(self):
-        cleaned_data = super().clean()
-        if "end" in cleaned_data and "start" in cleaned_data:
-            if cleaned_data["end"] <= cleaned_data["start"]:
-                self.add_error("end", _("The end time must be later than start time."))
-        return cleaned_data
-
-    class Meta:
-        model = Event
-        # Ensure 'calendars' is not in the exclude list
-        exclude = ("creator", "created_on")  # 'calendar' is no longer a direct field, so it's removed from exclude
-        widgets = {"color_event": ColorInput}
-
-
 class OccurrenceForm(SpanForm):
     class Meta:
         model = Occurrence
